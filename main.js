553f4923a1db85d637c8d0a5ca0645a1b6b5ba08
Tieg O'Sullivan <tiegosullivanpsnl@gmail.com>
2021-09-10 20:28:51 +0200

    :sparkles: updating timer

--- main.js
+++ main.js
@@ -1,59 +1,82 @@
-console.log('eyy')
-
 let habitZone = document.getElementsByClassName('habit-zone')[0];
+let resetButtons = document.getElementsByClassName('reset-button');
 
-habitZone.innerHTML = '' // TODO: is this shoddy?
+function refreshHabitZone() {
+    habitZone.innerHTML = '' // TODO: is this shoddy?
 
-habitData = {
-    0: {
-        title: 'P',
-        start: 'Thu Sep 08 2021 22:00:12 GMT+0200 (South Africa Standard Time)',
-    },
-    1: {
-        title: 'M',
-        start: 'Thu Sep 05 2021 22:00:12 GMT+0200 (South Africa Standard Time)',
-    },
-    2: {
-        title: 'O',
-        start: 'Thu Sep 02 2021 22:00:12 GMT+0200 (South Africa Standard Time)',
-    },
+    habitData = {
+        0: {
+            title: 'Porn',
+            start: 'Thu Sep 08 2021 22:00:12 GMT+0200 (South Africa Standard Time)',
+        },
+        1: {
+            title: 'Masturbation',
+            start: 'Thu Sep 05 2021 22:00:12 GMT+0200 (South Africa Standard Time)',
+        },
+        2: {
+            title: 'Orgasm',
+            start: 'Thu Sep 02 2021 22:00:12 GMT+0200 (South Africa Standard Time)',
+        },
+    }
+    // console.log(habitData)
+
+    for (key in habitData) {
+        // object construction
+        let habit = document.createElement('div');
+        habit.classList.add('habit');
+        
+        let habitTitle = document.createElement('div');
+        habitTitle.classList.add('habit-title')
+        
+        let habitTitleSpan = document.createElement('span');
+        habitTitleSpan.innerHTML = habitData[key]['title']
+        
+        let habitBody = document.createElement('div');
+        habitBody.classList.add('habit-body')
+        let count = document.createElement('div')
+        count.classList.add('count')
+        let today = new Date()
+        let d = new Date(habitData[key]['start'])
+        count.innerText = `${((today-d)/(1000*60*60*24)).toFixed(5)} days`
+
+
+        let habitReset = document.createElement('div')
+        habitReset.classList.add('reset-div')
+        let habitResetButton = document.createElement('button')
+        habitResetButton.classList.add('reset-button')
+        habitResetButton.innerText = 'Reset'
+        habitReset.appendChild(habitResetButton)
+
+        // child appending
+        habitTitle.appendChild(habitTitleSpan)
+        habit.appendChild(habitTitle)
+
+        habitBody.appendChild(count)
+        habitBody.appendChild(habitReset)
+        habit.appendChild(habitBody)
+
+        habitZone.appendChild(habit)
+
+
+    }
 }
-// console.log(habitData)
 
-for (key in habitData) {
-    // object construction
-    let habit = document.createElement('div');
-    habit.classList.add('habit');
-    
-    let habitTitle = document.createElement('div');
-    habitTitle.classList.add('habit-title')
-    
-    let habitTitleSpan = document.createElement('span');
-    habitTitleSpan.innerHTML = habitData[key]['title']
-    
-    let habitBody = document.createElement('div');
-    habitBody.classList.add('habit-body')
-    let count = document.createElement('div')
+refreshHabitZone()
+
+// function foo() {
+//     console.log('foo')
+// }
+
+// foo()
+
+function refreshCount(countDivs) {
     let today = new Date()
-    let d = new Date(habitData[key]['start'])
-    count.innerText = (today-d)/(1000*60*60*24)
-
-
-    let habitReset = document.createElement('div')
-    habitReset.classList.add('reset')
-    let habitResetButton = document.createElement('button')
-    habitResetButton.innerText = "Reset"
-    habitReset.appendChild(habitResetButton)
-
-    // child appending
-    habitTitle.appendChild(habitTitleSpan)
-    habit.appendChild(habitTitle)
-
-    habitBody.appendChild(count)
-    habitBody.appendChild(habitReset)
-    habit.appendChild(habitBody)
-
-    habitZone.appendChild(habit)
-
-
+    for (var i = 0; i < countDivs.length; i++) {
+        var d = new Date(habitData[key]['start'])
+        countDivs[i].innerText = `${((today-d)/(1000*60*60*24)).toFixed(5)} days`
+    }
 }
+
+// setInterval(refreshHabitZone, 1)
+countDivs = document.getElementsByClassName('count')
+setInterval(refreshCount, 1000, countDivs)
